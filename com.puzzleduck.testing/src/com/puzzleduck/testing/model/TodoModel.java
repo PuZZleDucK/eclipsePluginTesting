633838c0517782c51905aa8c5e09b4c9c5f83259
PuZZleDucK <puzzleduck@gmail.com>
2013-02-08 11:37:42 +1100

    add todo data model

---
+++ com.puzzleduck.testing/src/com/puzzleduck/testing/model/TodoModel.java
@@ -0,0 +1,32 @@
+package com.puzzleduck.testing.model;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class TodoModel {
+	private String summary;
+	private String description;
+
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+}
